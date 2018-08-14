d182acda46acd711c948baf564b630bce7f6432e
manmanma <1057899309@qq.com>
2018-08-14 23:17:07 +0800

    update user

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,21 @@ const filter = {_v:0,password:0}
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/user', function(req, res, next) {
+  // 得到请求cookie的userid
+  const userId = req.cookies.userId
+  if(!userId){
+    return res.send({code: 1, msg: '请先登陆'});
+  }
+  UserModel.findOne({_id:userId},filter,(err,userData) => {
+    if(userData){
+      return res.json({code:0,data:userData})
+    }else{
+      res.clearCookie('userId')
+      return res.send({code: 2, msg: '没有这个用户'});
+    }
+  })
+});
 router.post('/register',(req,res) => {
   //1.获取数据
   const {username,password,type} = req.body;
@@ -33,10 +48,28 @@ router.post('/login',(req,res)=>{
     if(userData){
       //cookie 注册后立即登陆 免登陆7天
       res.cookie('userId',userData._id,{maxAge:1000*60*60*24*7})
-      res.json({code:0,data:{_id:userData._id,username}})
+      res.json({code:0,data:{_id:userData._id,username,type:userData.type,header:userData.header}})
     }else{
       res.send({code: 1, msg: '用户名或密码错误'})
     }
+  })
+})
+// 3. 更新用户路由
+router.post('/update', function (req, res,next) {
+  // 得到请求cookie的userid
+  const userId = req.cookies.userId
+  if(!userId) {// 如果没有, 说明没有登陆, 直接返回提示
+    return res.send({code: 1, msg: '请先登陆'});
+  }
+  //更新数据库中对应的数据
+  UserModel.findByIdAndUpdate({_id: userId}, req.body, function (err, user) {// user是数据库中原来的数据
+    const {_id, username, type} = user
+    // node端 ...不可用
+    // const data = {...req.body, _id, username, type}
+    // 合并用户信息
+    const data = Object.assign(req.body, {_id, username, type})
+    // assign(obj1, obj2, obj3,...) // 将多个指定的对象进行合并, 返回一个合并后的对象
+    res.send({code: 0, data})
   })
 })
 module.exports = router;
