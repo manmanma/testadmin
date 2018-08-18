f4714a10c201adbe6405bb5e703b1c25c6e168d7
manmanma <1057899309@qq.com>
2018-08-19 00:06:21 +0800

    'update'

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const md5 = require('blueimp-md5')
-const {UserModel} = require('../db/models')
+const {UserModel, ChatModel} = require('../db/models')
 const router = express.Router()
 const filter = {_v:0,password:0}
 
@@ -8,6 +8,7 @@ const filter = {_v:0,password:0}
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+//根据userid获取用户的相关信息
 router.get('/user', function(req, res, next) {
   // 得到请求cookie的userid
   const userId = req.cookies.userId
@@ -23,6 +24,7 @@ router.get('/user', function(req, res, next) {
     }
   })
 });
+//根据类型获取用户列表
 router.get('/userlist',function (req,res,next) {
   const {type} = req.query
   UserModel.find({type},filter,(err, userData)=>{
@@ -33,6 +35,48 @@ router.get('/userlist',function (req,res,next) {
     }
   })
 })
+/*获取当前用户所有相关聊天信息列表*/
+router.get('/msglist', function (req, res) {
+  // 获取cookie中的userid
+  const userId = req.cookies.userId
+  // 查询得到所有user文档数组
+  UserModel.find(function (err, userDocs) {
+    // 用对象存储所有user信息: key为user的_id, val为name和header组成的user对象
+    const users = {} // 对象容器
+    userDocs.forEach(doc => {
+      users[doc._id] = {username: doc.username, header: doc.header}
+    })
+    /*
+    查询userid相关的所有聊天信息
+     参数1: 查询条件
+     参数2: 过滤条件
+     参数3: 回调函数
+    */
+    ChatModel.find({'$or': [{from: userId}, {to: userId}]}, filter, function (err, chatMsgs) {
+      // 返回包含所有用户和当前用户相关的所有聊天消息的数据
+      console.log(chatMsgs)
+      res.send({code: 0, data: {users, chatMsgs}})
+    })
+  })
+})
+/*修改指定消息为已读*/
+router.post('/readmsg', function (req, res) {
+  // 得到请求中的from和to
+  const from = req.body.from
+  const to = req.cookies.userId
+  /*
+  更新数据库中的chat数据
+  参数1: 查询条件
+  参数2: 更新为指定的数据对象
+  参数3: 是否1次更新多条, 默认只更新一条
+  参数4: 更新完成的回调函数
+   */
+  ChatModel.update({from, to, read: false}, {read: true}, {multi: true}, function (err, doc) {
+    console.log('/readmsg', doc)
+    res.send({code: 0, data: doc.nModified}) // 更新的数量
+  })
+})
+//注册
 router.post('/register',(req,res) => {
   //1.获取数据
   const {username,password,type} = req.body;
@@ -52,6 +96,7 @@ router.post('/register',(req,res) => {
     }
   })
 })
+//登录
 router.post('/login',(req,res)=>{
   const {username,password} = req.body;
   UserModel.findOne({username,password:md5(password)},filter,(err,userData) => {
@@ -64,7 +109,7 @@ router.post('/login',(req,res)=>{
     }
   })
 })
-// 3. 更新用户路由
+//更新用户信息
 router.post('/update', function (req, res,next) {
   // 得到请求cookie的userid
   const userId = req.cookies.userId
